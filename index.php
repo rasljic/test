59b846f3f8d76763a0b2f16da53467e463c056bd
Milan Rasljic <rasljic.milan@gmail.com>
2020-05-07 18:40:19 +0200

    Add the index file and update the gitignore file.

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+
+    <head>
+        <title>Test Xampp</title>
+    </head>
+
+    <body>
+
+        <?php
+
+        for($i=0; $i <20; $i++) {
+            echo"<h3>Test number #". $i ."</h3>";
+        }
+
+        ?>
+
+    </body>
+
+</html>
